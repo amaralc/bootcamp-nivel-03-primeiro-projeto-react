1a2a57bce60112648f6944f525897193c6807379
amaralc <amaral.calil@gmail.com>
2020-12-14 13:12:53 -0300

    FEATURE / Adiciona parametro na rota com sinal +

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,11 @@ import Repository from '../pages/Repository';
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={Dashboard} />
-    <Route path="/repository" component={Repository} />
+    {/**
+     * O sinal de + indica que o parametro é tudo o que vem depois do sinal ':'
+     * e ajuda a incluir parametros que contenham sinal de '/'
+     */}
+    <Route path="/repository/:repository+" component={Repository} />
   </Switch>
 );
 
